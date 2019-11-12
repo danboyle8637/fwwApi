01814962eeab604f2f0034b90bc0b4dfef5bd52a
Dan Boyle <danboyle8637@gmail.com>
2019-11-12 18:02:41 -0500

    Updated getUser so if a non-member clicks on the signup... the account will be deleted and they will be redirected to the sign up flow.

--- functions/handlers/getUser.js
+++ functions/handlers/getUser.js
@@ -1,25 +1,37 @@
-const db = require('../utils/admin').db
+const db = require("../utils/admin").db;
+const auth = require("../utils/admin").auth;
 
 exports.getUser = (req, res) => {
-  const userId = req.userId
+  const userId = req.userId;
 
-  db.collection('users')
+  db.collection("users")
     .doc(userId)
     .get()
     .then(userDoc => {
-      const userData = userDoc.data()
+      const userExists = userDoc.exists();
 
-      return res.status(200).json({
-        message: 'Successfully got user',
-        firstName: userData.firstName,
-        programs: userData.programs,
-        photoUrl: userData.photoUrl
-      })
+      if (userExists) {
+        const userData = userDoc.data();
+
+        return res.status(200).json({
+          message: "Successfully got user",
+          firstName: userData.firstName,
+          programs: userData.programs,
+          photoUrl: userData.photoUrl
+        });
+      } else {
+        auth.deleteUser(userId).then(() => {
+          res.status(400).json({
+            message: "Not a member yet. Sign Up!",
+            error: "not-member"
+          });
+        });
+      }
     })
     .catch(error => {
-      return res.status(404).json({
-        message: 'User not found.',
+      return res.status(500).json({
+        message: "User not found.",
         error: error
-      })
-    })
-}
+      });
+    });
+};
